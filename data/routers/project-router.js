c99ba97e86aab1a027eac80324cc8ee89007c1a0
Raine Wallace <rainebeaudubs@gmail.com>
2019-02-22 12:15:45 -0500

    Can now get projects by ID which also brings up all of its corresponding actions.

--- data/routers/project-router.js
+++ data/routers/project-router.js
@@ -19,4 +19,28 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const project = await ProjectDb.get(req.params.id);
+
+    if (project) {
+      res
+        .status(200)
+        .json(project)
+    } else {
+      res
+        .status(404)
+        .json({
+          message: 'Oh, shoot. This project could not found.'
+        }); 
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: 'Awwh shucks, there was an error retrieving this project.'
+      });
+  }
+});
+
 module.exports = router;
